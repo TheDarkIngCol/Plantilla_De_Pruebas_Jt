ad5180cdbe980af0c3f8543795d7646c35eb44a0
TheDarkIngCol <sukiblue591@gmail.com>
2025-06-16 16:42:41 -0500

    add suma

--- src/test/java/com/tests/JavaPractice/Aplication.java
+++ src/test/java/com/tests/JavaPractice/Aplication.java
@@ -5,6 +5,23 @@ import java.util.Scanner;
 public class Aplication {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    int opcion = scanner.nextInt();
+    System.out.println("Por favor, ingrese su nombre");
+    System.out.println("1. Saludar");
+    System.out.println("2. Contar hasta un número");
+    System.out.println("3. Clasificar edad");
+    System.out.println("4. Salir");
+    String nombre = scanner.nextLine();
+
+    switch (opcion) {
+      case 1:
+        
+        break;
+    
+      default:
+        break;
+    }
+
     }
   }
   
